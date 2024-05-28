46f91f26c99f1dbc9042b8d7e108760e589e8fc8
paolo151204 <epic353paolo@gmail.com>
2024-05-28 14:56:52 +0100

    background changes

--- lab1/task_1a/src/task_1a/task_123A.java
+++ lab1/task_1a/src/task_1a/task_123A.java
@@ -5,6 +5,6 @@ public class task_123A {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello, word");
+		System.out.println("this is change to the program. ");
 	}
-
 }
