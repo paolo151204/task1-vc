1764af3eb67a81ded374ef2a3838efa0bebe987b
paolo151204 <epic353paolo@gmail.com>
2024-05-28 14:46:08 +0100

    first push

---
+++ lab1/src/lab1/Lab1.java
@@ -0,0 +1,13 @@
+package lab1;
+
+public class Lab1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+				System.out.println("hello, word");
+			
+		
+	}
+
+}
---
+++ lab1/task_1a/src/task_1a/task_123A.java
@@ -0,0 +1,10 @@
+package task_1a;
+
+public class task_123A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello, word");
+	}
+
+}
